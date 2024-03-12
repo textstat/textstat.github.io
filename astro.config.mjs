26953d24ed155478e095b8f84db2665c1319b5b7
Alex Ward <alxwrd@googlemail.com>
2024-03-12 15:56:33 +0000

    initial implementation

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://textstat.org",
+  integrations: [tailwind(), mdx(), sitemap()],
+});
