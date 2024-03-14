47974e68ae0580c3af31d6f217f14a34cd5f3698
Alex Ward <alxwrd@googlemail.com>
2024-03-14 08:58:22 +0000

    remove team collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,5 @@
-// 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
-// 2. Define your collection(s)
 const blogCollection = defineCollection({
   schema: z.object({
     draft: z.boolean(),
@@ -12,28 +10,13 @@ const blogCollection = defineCollection({
       alt: z.string(),
     }),
     publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
+    author: z.string().default("Astroship"),
     category: z.string(),
     tags: z.array(z.string()),
   }),
 });
 
-const teamCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    name: z.string(),
-    title: z.string(),
-    avatar: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
-  }),
-});
 
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
 export const collections = {
-  'blog': blogCollection,
-  'team': teamCollection,
+  "blog": blogCollection,
 };
